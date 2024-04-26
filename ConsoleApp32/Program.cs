c84c73ff8538b0280d933916a40f22e16ca70748
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-04-26 13:29:39 +0400

    Task finished

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "Ali", "Vali", "Mammad", "Hasan", "Aytac", "Rena", "Sevgul", "Ramin", "Xurma" };
+
+            Console.WriteLine("Bir char daxil edin:");
+            char c = Console.ReadKey().KeyChar;
+
+            foreach (var item in strings)
+            {
+                foreach (char ch in item)
+                {
+                    if (ch.ToString().ToLower() == c.ToString().ToLower())
+                    {
+
+                        Console.WriteLine($"Element: {item}, İndeks: {Array.IndexOf(strings, item)}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
